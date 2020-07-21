7a1600e82ef1f35bc2f72d066d963591177cd4e8
Josselin Francois Downey <jossfrancois@gmail.com>
2020-07-21 22:28:52 +1000

    Improve anime search command
    anime command now returns a nice embed message instead of a simple link when a specific title has been chosen from the list.

--- cogs/mal_scraper.py
+++ cogs/mal_scraper.py
@@ -8,19 +8,69 @@ class mal_scraper(commands.Cog):
     def __init__(self, bot):
         self.bot=bot
 
+    """
+    Optimally the whole scraping should be a background process for ALL animes (figure out how to do that)
+    and saves the scraped info that we want (score, title, links, ...) in a DB, and
+    use the DB to build the embed, search the anime by name etc... so the command 
+    runs faster. This is fine for now given the scope of the bot
+    """
+
+    def build_embed_from_url(self, url: str):
+        result = requests.get(url)
+        src = result.content
+        soup = BeautifulSoup(src, "lxml")
+
+        title = soup.find("span", attrs={ "itemprop" : "name" }).contents[0]
+        description = soup.find("span", attrs={ "itemprop" : "description" }).text
+        image = soup.find("img", attrs={ "alt" : "{}".format(title)})['data-src']
+
+        # Border element contains most of the info we need, saving this part of the
+        # Html will save some time on the multiple find() calls below
+        border = soup.find("td", attrs={ "class" : "borderClass", "valign" : "top" })
+        
+        def border_text_elem(text: str):
+            return border.find("span", attrs={ "class" : "dark_text" }, text=text)
+
+        studios_arr = ["[{}](https://myanimelist.net{})".format(a.text, a['href']) for a in border_text_elem("Studios:").parent.find_all("a")]
+        studios = ", ".join(studios_arr)
+        
+        score = border_text_elem("Score:").find_next_sibling('span').contents[0]
+        
+        episodes = border_text_elem("Episodes:").parent.contents[2].strip()
+        status = border_text_elem("Status:").parent.contents[2].strip()
+        aired = border_text_elem("Aired:").parent.contents[2].strip()
+
+        embed = discord.Embed(title=title)
+        embed.url = url
+        embed.colour = 0x2f52a2
+
+        embed.set_thumbnail(url=image)
+        embed.add_field(name="Score", value=score, inline=False)
+        embed.add_field(name="Episodes", value=episodes, inline=True)
+        embed.add_field(name="Status", value=status, inline=True)
+        embed.add_field(name="Studios", value=studios, inline=True)
+        embed.add_field(name="Aired", value=aired, inline=True)
+
+        embed.description = description
+
+        return embed
 
     @commands.command()
     async def anime(self, ctx, *, name):
-        m = await ctx.send("On it...")
+        m = await ctx.send("Working on it...")
         ANIME_URL = "https://myanimelist.net/anime.php?q=" + name
         result = requests.get(ANIME_URL)
-        # print("Sending request to", DOFUS_PORTAL_URL)
-
+        # print("Sending request to", ANIME_URL)
         # print("Status code:", result.status_code)
 
+        if result.status_code != 200:
+            embed = discord.Embed(title="Error")
+            embed.description = "Can't get a response from **MyAnimeList** (Status Code {}). The site might be under maintenance. \nTry again later or check with a @Hackers.".format(result.status_code)
+            embed.colour = 0xcc0000
+            await ctx.send(embed=embed)
+            return
+
         src = result.content
-
-
         soup = BeautifulSoup(src, "lxml")
 
         animes = []
@@ -29,14 +79,14 @@ class mal_scraper(commands.Cog):
             if(tr.find("strong") is not None and len(animes) < 5):
                 animes.append([tr.find("strong").contents[0], tr.find('a')['href']])
         embed = discord.Embed(title='Search results for **{}**'.format(name))
+        embed.colour = 0x2f52a2
 
         list_text = ''
-
-
         for i in range(len(animes)):
             list_text += "{}. {}\n".format(i+1, animes[i][0])
 
         embed.description = list_text
+        embed.description += "\n**Choose an entry from the list (1 to 5)**"
         embed.set_thumbnail(url="https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png")
 
         def check(m):
@@ -44,22 +94,19 @@ class mal_scraper(commands.Cog):
 
         await m.delete()
         await ctx.send(embed=embed)
-        await ctx.send("**Choose an entry from the list (1 to 5)**")
         reply = await self.bot.wait_for("message", check=check)
         try:
             int(reply.content)
             if int(reply.content) > 0:
-                await ctx.send(animes[int(reply.content)-1][1])
-        except:
+                m = await ctx.send("Working on it...")
+                await ctx.send(embed=self.build_embed_from_url(animes[int(reply.content)-1][1]))
+                await m.delete()
+        except ValueError:
              await ctx.send("This ain't a number")
              
-        
-
         return animes
 
-
+  
 def setup(bot):
     bot.add_cog(mal_scraper(bot))
-# return list in embed
-# listen for message (number in the list)
-# embed details about said anime + link
+
