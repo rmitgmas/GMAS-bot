8eea977f1812fecd5bb5cc6583b5eb93b281c4d3
Josselin Francois Downey <jossfrancois@gmail.com>
2020-07-22 17:00:53 +1000

    Improved anime details embed message

--- cogs/mal_scraper.py
+++ cogs/mal_scraper.py
@@ -16,18 +16,22 @@ class mal_scraper(commands.Cog):
     """
 
     def build_embed_from_url(self, url: str):
+
         result = requests.get(url)
         src = result.content
         soup = BeautifulSoup(src, "lxml")
 
-        title = soup.find("span", attrs={ "itemprop" : "name" }).contents[0]
+        jp_title = soup.find("span", attrs={ "itemprop" : "name" }).contents[0]
+        en_title = getattr(soup.find("span", attrs={ "class" : "title-english" }), "text", None)         
+        title = en_title or jp_title
+
         description = soup.find("span", attrs={ "itemprop" : "description" }).text
-        image = soup.find("img", attrs={ "alt" : "{}".format(title)})['data-src']
+        image = soup.find("img", attrs={ "alt" : "{}".format(jp_title)})['data-src']
 
         # Border element contains most of the info we need, saving this part of the
         # Html will save some time on the multiple find() calls below
         border = soup.find("td", attrs={ "class" : "borderClass", "valign" : "top" })
-        
+    
         def border_text_elem(text: str):
             return border.find("span", attrs={ "class" : "dark_text" }, text=text)
 
@@ -39,17 +43,25 @@ class mal_scraper(commands.Cog):
         episodes = border_text_elem("Episodes:").parent.contents[2].strip()
         status = border_text_elem("Status:").parent.contents[2].strip()
         aired = border_text_elem("Aired:").parent.contents[2].strip()
-
+        
+        genres_arr = ["[{}](https://myanimelist.net{})".format(a.text, a['href']) for a in border_text_elem("Genres:").parent.find_all("a")]
+        genres = ", ".join(genres_arr)
+        
         embed = discord.Embed(title=title)
         embed.url = url
         embed.colour = 0x2f52a2
-
         embed.set_thumbnail(url=image)
         embed.add_field(name="Score", value=score, inline=False)
         embed.add_field(name="Episodes", value=episodes, inline=True)
-        embed.add_field(name="Status", value=status, inline=True)
+        embed.add_field(name="Genres", value=genres, inline=True)
         embed.add_field(name="Studios", value=studios, inline=True)
+        embed.add_field(name="Status", value=status, inline=True)
+        if border_text_elem("Premiered:"):
+            premiered_link_elem = border_text_elem("Premiered:").find_next_sibling('a')
+            premiered = "[{}]({})".format(premiered_link_elem.text, premiered_link_elem['href'])
+            embed.add_field(name="Premiered", value=premiered, inline=True)
         embed.add_field(name="Aired", value=aired, inline=True)
+
 
         embed.description = description
 
@@ -92,15 +104,15 @@ class mal_scraper(commands.Cog):
         def check(m):
             return m.author == ctx.message.author and  ctx.message.channel == m.channel
 
-        await m.delete()
         await ctx.send(embed=embed)
+        await m.delete()
         reply = await self.bot.wait_for("message", check=check)
         try:
             int(reply.content)
             if int(reply.content) > 0:
-                m = await ctx.send("Working on it...")
+                m1 = await ctx.send("Getting anime details...")
                 await ctx.send(embed=self.build_embed_from_url(animes[int(reply.content)-1][1]))
-                await m.delete()
+                await m1.delete()
         except ValueError:
              await ctx.send("This ain't a number")
              
